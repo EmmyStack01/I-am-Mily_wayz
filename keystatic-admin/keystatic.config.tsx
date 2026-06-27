8ad5456ddf9e33ea717da3984c7ac4974c9430de
EmmyStack01 <wezuduemoih@gmail.com>
2026-06-27 05:28:35 -0700

    Update keystatic.config.tsx

--- keystatic-admin/keystatic.config.tsx
+++ keystatic-admin/keystatic.config.tsx
@@ -57,8 +57,9 @@ export default config({
           }),
           profilePicture: fields.image({
             label: 'Profile Picture',
-            directory: 'keystatic-admin/public/profile',
-            publicPath: '/profile/',
+            directory: 'keystatic-admin/public',
+            publicPath: '/mily-wayz/',
+            //ADDED /mily-wayz/ PATH SO PROFILE PICTURE WILL SHOW UP ON https://emmystack01.com/mily-wayz, ALSO ADDED IT IN data.json profilePicture
           }),
         }, { label: 'Profile' }),
 
